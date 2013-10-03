ad181cd22630ecb6735b55b80105d21b97655f37
Victor <victor@victor-N61Jq.(none)>
2013-10-03 17:19:01 +0100

    Se conecta a thin

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 
 gem 'twitter'
 gem 'faraday', '0.8.7'
-gem 'rack'
-gem 'thin'
+gem 'gchartrb'
--- TwitterRake.rb
+++ TwitterRake.rb
@@ -1,18 +1,30 @@
 require 'rubygems'
 require 'twitter'
+require './configure'
 require 'rack'
 require 'thin'
-require './configure'
 
 
 
-body = lambda do |env|
+
+app = lambda do |env|
+
   req = Rack::Request.new env
   res = Rack::Response.new
 
+  #Comprobar que el pathinfo de la url es el correcto
   if req.path_info == '/TwitRake'
-
+    res.write <<-"EOS"
+      <p>Ha accedido correctamente a su página web</p>
+    EOS
+  else
+    res.write <<-"EOS"
+      <p>Escriba bien la url</p>
+    EOS
+  end
+  res.finish
 end
 
 
 
+Rack::Handler::Thin.run app, :Port=>9297
