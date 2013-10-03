8e8294ea5b8c282077b6b29d1a7b57d2d6857124
Victor <victor@victor-N61Jq.(none)>
2013-10-03 19:42:51 +0100

    Limpiando

--- configure.rb
+++
@@ -1,6 +0,0 @@
-Twitter.configure do |config|
-  config.consumer_key = 'uxj4i2qoTNZ4OWsvIwnijQ'
-  config.consumer_secret = '7tm7Kp0sJjRoUqPKZU9fj9OhFIGvb5MZktB9K5LZA'
-  config.oauth_token = '857743416-9WWFZTCO7FpoHuowPflDhvmglgawxFMg49klQ9Yj'
-  config.oauth_token_secret = 'Rdu4apPev7LqCfoTSElHj26yRzwd0YwwHakTOZhY'
-end
